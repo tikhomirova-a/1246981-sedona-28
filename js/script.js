e3b05a02cf9bb164546dd15285430a3210121c0f
tikhomirova-a <tikhomirova.aa95@gmail.com>
2020-07-09 19:31:32 +0300

    fix some errors, add the error animation, remove all the z-indexes in the modal form

--- js/script.js
+++ js/script.js
@@ -9,7 +9,22 @@ searchLink.addEventListener("click", function (evt) {
     modalForm.classList.remove("modal-hide");
     modalForm.classList.add("modal-show"); // добавляет анимацию
   } else {
-    modalForm.classList.add("modal-hide");
+    modalForm.classList.add("modal-hide"); // скрывает форму
     modalForm.classList.remove("modal-show");
+    modalForm.classList.remove("modal-error");
+  }
+});
+
+var inputs = modalForm.querySelectorAll("input");
+var buttonSearch = modalForm.querySelector(".search-form-button");
+
+buttonSearch.addEventListener("click", function (evt) {
+  modalForm.classList.remove("modal-error");
+  modalForm.offsetWidth = modalForm.offsetWidth; // позволяет убрать и снова добавить .modal-error
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].value == "") {
+      evt.preventDefault();
+      modalForm.classList.add("modal-error");
+    }
   }
 });
